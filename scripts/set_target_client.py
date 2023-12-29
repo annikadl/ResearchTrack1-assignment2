3c5970ba9e943bca457a9887431b4375342c889f
annikadl <annikadelucchi25@gmail.com>
2023-12-29 15:26:49 +0000

    working user interface

--- scripts/set_target_client.py
+++ scripts/set_target_client.py
@@ -16,11 +16,42 @@ from assignment_2_2023.msg import PlanningAction
 from assignment_2_2023.msg import PlanningAction, PlanningGoal, PlanningResult
 from std_msgs.msg import String
 
+rospy.init_node('set_target_client')
+
+###### PUBLISHER
+def publisher_node(msg):
+    # Create a publisher
+    pub = rospy.Publisher('pub_position_velocity', Vel, queue_size=10) 
+    
+    # Get the actual position and velocity
+    actual_pos = msg.pose.pose.position
+    actual_vel_linear = msg.twist.twist.linear
+    actual_vel_angular = msg.twist.twist.angular
+    
+    
+    # Initialize the ROS node
+    #rospy.init_node('publisher_node', anonymous=True)
+
+    # Set the publishing rate (e.g., 1 Hz)
+    rate = rospy.Rate(1)  # 1 Hz
+    
+    while not rospy.is_shutdown():
+    	my_pos_and_vel = Vel()
+    	#my_pos_and_vel_str = "My position and velocity is %s" % str(my_pos_and_vel)
+    	
+    	my_pos_and_vel.pos_x = actual_pos.x
+    	my_pos_and_vel.pos_y = actual_pos.y
+    	my_pos_and_vel.vel_x = actual_vel_linear.x
+    	my_pos_and_vel.vel_z = actual_vel_angular.z
+    	
+    	#rospy.loginfo(my_pos_and_vel)
+    	pub.publish(my_pos_and_vel)
+    	rate.sleep()
+
 
 ####### CLIENT
 def parameters_client_main():
-    rospy.init_node('set_target_client')
-
+    
     # Create an action client
     client = actionlib.SimpleActionClient('/reaching_goal', assignment_2_2023.msg.PlanningAction)
     client.wait_for_server()
@@ -63,8 +94,8 @@ def parameters_client_main():
 
         # Cancel the goal -> desired values go to zero? or to random ones?
         if command == 'c':
-            rospy.set_param('/des_pos_x', 0.0)
-            rospy.set_param('/des_pos_y', 0.0)
+            client.cancel_goal()
+            print("Goal cancelled")
 
         rospy.loginfo("Received goal: des_x = %f, des_y = %f", goal.target_pose.pose.position.x, goal.target_pose.pose.position.y)
 
@@ -76,10 +107,10 @@ def main():
     	#global pub
     
     	# PUBLISHER: send a message which contains two parameters (velocity and position)
-    	#pub = rospy.Publisher("/pos_vel", Vel, queue_size = 1)
+    	pub = rospy.Publisher("/pos_vel", Vel, queue_size = 1)
     
     	# SUBSCRIBER: get from "Odom" two parameters (velocity and position)
-    	#sub_from_Odom=rospy.Subscriber("/odom",Odometry,publisher_node)
+    	sub_from_Odom=rospy.Subscriber("/odom",Odometry,publisher_node)
     
     	# Calling the function client
     	parameters_client_main()
