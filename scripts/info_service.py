bd84a655a4cdf56e86d8585a3f31d270f0b8345c
annikadl <annikadelucchi25@gmail.com>
2023-12-31 09:50:51 +0000

    info_service correctly working as a service

--- scripts/info_service.py
+++ scripts/info_service.py
@@ -17,6 +17,7 @@ from assignment_2_2023.msg import PlanningAction, PlanningGoal, PlanningResult
 from std_msgs.msg import String
 from std_srvs.srv import Empty, EmptyResponse, SetBool, SetBoolResponse
 from actionlib_msgs.msg import GoalStatusArray
+from assignment_2_2023.srv import info_service, info_serviceResponse
 
 # Another service node thatsubscribes to the robot's position and velocity (using the custom message) and implements a server to retrieve the distance of the robot from the target and the robot's average speed.
 
@@ -25,7 +26,7 @@ success = False
 #velocity_window_size = 10  # Default window size
 
 def get_distance_and_averagevelocity(msg):
-    global success
+    global success, average_vel_x, distance
 
     # desired position - actual position
     des_x = rospy.get_param('/des_pos_x')
@@ -34,14 +35,14 @@ def get_distance_and_averagevelocity(msg):
     actual_x = msg.pos_x
     actual_y = msg.pos_y
     
-    rospy.loginfo("Desired position: des_x = %f, des_y = %f", des_x, des_y)
-    rospy.loginfo("Actual position: actual_x = %f, actual_y = %f", actual_x, actual_y)
+    #rospy.loginfo("Desired position: des_x = %f, des_y = %f", des_x, des_y)
+    #rospy.loginfo("Actual position: actual_x = %f, actual_y = %f", actual_x, actual_y)
     
     des_coordinates = [des_x, des_y]
     actual_coordinates = [actual_x, actual_y]
     distance = math.dist(des_coordinates, actual_coordinates)
     
-    rospy.loginfo("Actual distance from target: distance = %f", distance)
+    #rospy.loginfo("Actual distance from target: distance = %f", distance)
 
     # Compute average velocity along x-axis
     if isinstance(msg.vel_x, list):
@@ -51,7 +52,7 @@ def get_distance_and_averagevelocity(msg):
 
     average_vel_x = sum(vel_data) / min(len(vel_data), velocity_window_size)
 
-    rospy.loginfo("Average velocity: %f", average_vel_x)
+    #rospy.loginfo("Average velocity: %f", average_vel_x)
 
     if success:
         rospy.loginfo("Goal successfully reached")
@@ -60,7 +61,13 @@ def get_distance_and_averagevelocity(msg):
 def result_callback(msg):
     global success
     if msg.status_list and msg.status_list[-1].status == 3:  # Check if the last status is SUCCEEDED
-        success = True        
+        success = True    
+        
+def get_values(s):      
+    global average_vel_x, distance
+    
+    rospy.loginfo("Distance= %f Average velocity = %f", distance, average_vel_x)
+    return info_serviceResponse(distance, average_vel_x)			      
 
 def info_service_main():
     global success, velocity_window_size
@@ -70,6 +77,9 @@ def info_service_main():
 
     # Subscribe to the custom message
     subscriber = rospy.Subscriber("/pos_vel", Vel, get_distance_and_averagevelocity)
+    
+    # Service
+    service = rospy.Service("info_service", info_service, get_values)
     
     # Subscribe to action result
     #result_subscriber = rospy.Subscriber("/reaching_goal/result", GoalStatusArray, result_callback)
