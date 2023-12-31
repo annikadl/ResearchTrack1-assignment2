964865aa77f579dde356c9a105c42d7ab076ec14
annikadl <annikadelucchi25@gmail.com>
2023-12-31 10:36:09 +0000

    last_target_service correctly returns last target

--- scripts/last_target_service.py
+++ scripts/last_target_service.py
@@ -16,34 +16,48 @@ from assignment_2_2023.msg import PlanningActionGoal
 from assignment_2_2023.msg import PlanningAction, PlanningGoal, PlanningResult
 from std_msgs.msg import String
 from std_srvs.srv import Empty, EmptyResponse
+from assignment_2_2023.srv import Last_target, Last_targetResponse
 
-global last_des_x, last_des_y
-last_des_x = 0.0
-last_des_y = 1.0
+
+#last_des_x = 0.0
+#last_des_y = 1.0
 
 #def goal_callback(msg):
 #    last_des_x = msg.target_pose.pose.position.x
 #    last_des_y = msg.target_pose.pose.position.y
 
-def get_last_target(request):
+def get_last_target(msg):
     # Use EmptyResponse as the response type
-    response = EmptyResponse()
+    #response = EmptyResponse()
+    
+    global last_des_x, last_des_y
+
     
     last_des_x = rospy.get_param('/des_pos_x')
     last_des_y = rospy.get_param('/des_pos_y')
     
-    print("Last input target is des_x = %f, des_y = %f" % (last_des_x, last_des_y))
+    #print("Last input target is des_x = %f, des_y = %f" % (last_des_x, last_des_y))
+    
+    
+def result_callback(s):
+    global last_des_x, last_des_y 
+    
+    response = Last_targetResponse()
+    response.last_target_x = last_des_x
+    response.last_target_y = last_des_y
     
     return response
+    	    
 
 def last_target_service():
     rospy.init_node('last_target_server')
 
     # Subscribe to the correct action goal topic
-    #rospy.Subscriber('/reaching_goal/goal', PlanningActionGoal, goal_callback)
+    rospy.Subscriber("/pos_vel", Vel, get_last_target)
     
     # Use Empty service type
-    service = rospy.Service('last_target', Empty, get_last_target)
+    service = rospy.Service('last_target', Last_target, result_callback)
+    #print("Last input target is des_x = %f, des_y = %f" % (last_des_x, last_des_y))
     rospy.spin()
 
 if __name__ == "__main__":
